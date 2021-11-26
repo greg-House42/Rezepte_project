bb715cd64b0df2e8d2af4a326a7196aa51f69ebd
greg-House42 <micha.epple@gmx.de>
2021-11-26 12:31:11 +0100

    all new

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -1,70 +1,49 @@
 @extends('dashboard')
 
-    <!DOCTYPE html>
-        <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-            <head>
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('content')
+    <main class="signup-form">
+        <div class="container mx-px">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h3 class="card-header text">Rezepte hochladen:</h3>
+                            <div class="card-body">
+                                <form action="{{ route('recipes.store') }}" method="post" enctype="multipart/form-data">
+                                    <!-- Add CSRF Token -->
+                                    @csrf
+                                    <div class="form-group">
+                                        <label>Titel:</label></br>
+                                        <input type="text" class="form-control" name="titel"></br>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Rezept:</label></br>
+                                        <input type="text" class="form-control" name="description"></br>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Zutaten:</label></br>
+                                        <input type="text" class="form-control" name="ingredients"></br>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <input type="file" name="files[]" placeholder="Choose files" multiple >
+                                            </div>
+                                            @error('files')
+                                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                            @enderror
+                                        </div>
 
-                <title><Micha's Rezepte Ecke></title>
-
-                <!-- Fonts -->
-                <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-                <style>
-                    body {
-                        font-family: 'Nunito', serif;
-                    }
-                </style>
-            </head>
-        <body>
-
-        <!-- if validation in the controller fails, show the errors -->
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
-            <div>
-                <form action="{{ route('recipes.store') }}" method="post" enctype="multipart/form-data">
-                    <!-- Add CSRF Token -->
-                    @csrf
-                    <div class="form-group">
-                        <label>Titel</label>
-                        <input type="text" class="form-control" name="titel">
-                    </div>
-                    <div class="form-group">
-                        <label>Rezept</label>
-                        <input type="text" class="form-control" name="description">
-                    </div>
-                    <div class="form-group">
-                        <label>Zutaten</label>
-                        <input type="text" class="form-control" name="ingredients">
-                    </div>
-                    <div class="row">
-
-                        <div class="col-md-12">
-                            <div class="form-group">
-                                <input type="file" name="files[]" placeholder="Choose files" multiple >
+                                        <br class="col-md-12">
+                                            <button type="submit" class="btn btn-primary" id="submit">Submit</button></br>
+                                        </div>
+                                        <li class="nav-item">
+                                            </br><a class="nav-link" href="/recipes/list">Rezepte zum Nachkochen - hier klicken</a>
+                                        </li>
+                                </form>
                             </div>
-                            @error('files')
-                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-                        <div class="col-md-12">
-                            <button type="submit" class="btn btn-primary" id="submit">Submit</button>
-                        </div>
                     </div>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/recipes/list">Rezepte</a>
-                    </li>
-                </form>
+                </div>
             </div>
-        </body>
-    </html>
+        </div>
+    </main>
+@endsection
