fe1ebe53382183da803f7238f59b4037122c443b
greg-House42 <micha.epple@gmx.de>
2021-12-06 15:40:07 +0100

    all new

--- resources/views/recipes/list.blade.php
+++ resources/views/recipes/list.blade.php
@@ -12,7 +12,7 @@
                         <div class="row justify-content-center" align="center">
                             <div class="col-md-4">
                                 <div class="card">
-                                    <h6 class="text-4xl font-bold text-white text" align="center">Rezepte zum Nachkochen3:</h6>
+                                    <h6 class="text-6xl font-bold text-white text" align="center"><u><i>Rezepte zum Nachkochen:</i></u></h6>
                                     <div class="card-body" align="center">
                                         <form action="{{ route('recipes.index') }}" method="get" enctype="multipart/form-data">
                                             <!-- Add CSRF Token -->
@@ -21,27 +21,27 @@
                                                 <tr align="center">
                                                     <p class="text-4xl font-bold text-white text-center" align="center">
                                                     <div class="form-group">
-                                                        </br><p class="text-4xl font-bold text-white text-center">Nummer:    </p>
-                                                        <td>{{$recipe->id}}</td></br>
+                                                        </br><p class="text-2xl font-bold text-white text-center">Rezept-Nummer:    </p>
+                                                        <td><u>{{$recipe->id}}</u></td></br>
                                                     </div>
                                                     <div class="form-group">
-                                                        <p class="text-4xl font-bold text-white text" align="center">Titel:    </p>
+                                                        <p class="text-2xl font-bold text-white text" align="center">Titel:    </p>
                                                         <center>
                                                             <td>{{$recipe->titel}}</td></br>
                                                         </center>
                                                     </div>
                                                     <div class="form-group">
-                                                        <p class="text-4xl font-bold text-white text" align="center">Rezept:    </p>
+                                                        <p class="text-2xl font-bold text-white text" align="center">Rezept:    </p>
                                                         <td>{{$recipe->description}}</td></br>
                                                     </div>
                                                     <div class="form-group" align="center">
                                                         <p style="text-align:center">
-                                                            <div class="text-4xl font-bold text-white text" align="center">Zutaten:    </div>
+                                                            <div class="text-2xl font-bold text-white text" align="center">Zutaten:    </div>
                                                             <p class align="center">{{$recipe->ingredients}}</p></br>
                                                         </p>
                                                     </div>
                                                     <div class="container mx-auto space-y-2 lg:space-y-0 lg:gap-2 lg:grid lg:grid-cols-5">
-                                                        <p class="text-4xl font-bold text-white text content-center" align="center">Bilder:    </p>
+                                                        <p class="text-2xl font-bold text-white text content-center" align="center">Bilder:    </p>
                                                         @foreach($recipe->images as $image)
                                                             <div class="form-group">
                                                                 <div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'>
